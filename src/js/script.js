617531831b8857ed88e10174ec21b1f0a38ab3cf
maxvojcuk <maksimvojcuk09@gmail.com>
2024-04-08 20:09:53 +0300

    Зробленно

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+//1
+const celsius = 24;
+const farenheit = celsius * 1.8 + 32;
+console.log(farenheit);
+//2
+const day = 30;
+const hoursInMonth = day * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log("hour", hoursInMonth);
+console.log("minute", minutesInMonth);
+//3
+const heals = 100;
+const energy = 200;
+const healsTwo = heals - 40;
+const energyTwo = energy - 150;
+console.log(healsTwo);
+console.log(energyTwo);
+//4
+const sum = 100;
+const dicount = 0.1;
+const discountedAmount = sum - sum * dicount;
+console.log(discountedAmount);
+//5
+const number = 5.6;
+const floor = Math.floor(number);
+console.log(floor);
+//6
+let str = "3.14";
+let num = parseFloat(str);
+console.log(num);
+
+//7
+const stringNumber = "10.22";
+const numberTwo = parseInt(stringNumber);
+console.log(numberTwo);
+//8
+const firstNumber = 50;
+const secondNumber = Math.sqrt(firstNumber);
+console.log(secondNumber);
+//9
+const numberString = "20fd";
+const parNumber = parseInt(numberString);
+console.log(paarNumber);
+
+const numberSecond = 10000;
+const strNumber = toString(numberSecond);
+console.log(typeof strNumber);
